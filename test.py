14d8f059bf5088c4ec12c867a468eb081c30e4bb
Samuel Oseh <samueloseh007@gmail.com>
2021-03-05 23:23:30 +0100

    Added Prediction Pipeline and Training Pipeline

--- test.py
+++ test.py
@@ -1,17 +1,20 @@
 import requests
 
 BASE = 'https://epl-predictions-api.herokuapp.com/'
-
+LOCAL_HOST = 'http://127.0.0.1:5000/'
 data = {
     'HomeTeam': 'Fulham',
     'AwayTeam': 'Arsenal',
-    'AttackDiff': 0.463318,
-    'DefenceDiff': 0.475107,
-    'ExpGoalDiff': 0.074259,
-    'PastGoalDiff': 0.234008,
-    'PastShotsOnTargetDiff': -0.349808,
-    'PastShotsDiff': -0.804848,
+    'HAS': 0.463318,
+    'AAS': 5.475107,
+    'HDS': 0.074259,
+    'ADS': 5.234008,
+    'HXG': 0.349808,
+    'AXG': 2.804848,
+    'PastGoalDiff': -0.222,
+    'PastShotsDiff': -0.89,
+    'PastShotsOnTargetDiff': -0.900
 }
 
-response = requests.get(BASE + 'predict', data)
+response = requests.get(LOCAL_HOST + 'predict', data)
 print(response.json())
